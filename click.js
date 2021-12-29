459475b2ecf7f2a9f7ab4b52bfdbf63620054820
showboat051 <rashad.davis87@yahoo.com>
2021-12-29 02:44:28 -0600

    added button click

--- click.js
+++ click.js
@@ -1,2 +1,17 @@
 //*****************UNIT RESOURCES************************** */
-const 
+const Buttons = document.querySelectorAll('.buttons');
+const countDisplay = document.getElementById(".numDisplay");
+
+//**********VARIABLES************* */
+var cd = 0;
+
+
+// button clicks
+for (var i = 0; i < Buttons.length; i++) {
+    Buttons[i].addEventListener("click" , function(e) {
+      document.getElementById(".numDisplay").value = cd;
+        if(e.target.value === "+") {
+            cd++;
+        }
+    })
+}
