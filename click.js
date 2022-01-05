bb60764a2b479b1746420983746325e76151a53c
showboat051 <rashad.davis87@yahoo.com>
2022-01-05 02:15:34 -0600

    css rules work

--- click.js
+++ click.js
@@ -9,13 +9,25 @@ for (var i = 0; i < Buttons.length; i++) {
     Buttons[i].addEventListener("click" , function(e) {
         if(e.target.value === "+") {
             countDisplay.value = parseInt(countDisplay.value) + 1;
-            console.log("clicked");
+            if (countDisplay.value > 0) {
+                chnagetoGold();
+            }
+            
         }
+
         if (e.target.value === "-") {
             countDisplay.value = parseInt(countDisplay.value) - 1;
-            changeToBlue();
+            if (countDisplay.value < 0) {
+                changeToBlue();
+            }
+            
         }
     })
+}
+
+
+function chnagetoGold () {
+    document.body.style.background = "gold";
 }
 
 function changeToBlue () {
